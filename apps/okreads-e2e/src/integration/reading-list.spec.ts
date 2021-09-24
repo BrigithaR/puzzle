49d452b83bd326cf08218aac29dabe77294c0c48
Brigitha <brigithajessy@gmail.com>
2021-09-24 10:29:45 +0530

    undo action implemented

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -3,24 +3,26 @@ describe('When: I use the reading list feature', () => {
     cy.startAt('/');
   });
 
-  it('Then: I should see my reading list', () => {
-    cy.get('[data-testing="toggle-reading-list"]').click();
-
-    cy.get('[data-testing="reading-list-container"]').should(
-      'contain.text',
-      'My Reading List'
-    );
-  });
-
   it('Then: I should undo the added book from reading list',()=>{
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('body').then((body) => {
+      if (body.find('[data-testing="removebutton"]').length > 0) {
+        cy.get('[data-testing="removebutton"]').each(($el) => {
+          cy.wrap($el).click();
+        });
+      }
+    });
+    cy.get('[data-testing="close-reading-list"]').click();
     cy.get('input[type="search"]').type('java');
 
     cy.get('form').submit();
 
-    cy.contains('Effective Java');
-    
-    cy.contains('Want to Read').click();
-    
-    cy.contains('Undo').click().should('be.enabled','Want to Read');
+    cy.get('[data-testing="want-to-read-button"]').first().click();
+
+    cy.wait(2000);
+
+    cy.contains('Undo').click();
+
+    cy.get('[data-testing="reading-item"]').should('have.length', 0);
    });
 });
